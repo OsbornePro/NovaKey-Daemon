a015615cd85df1e7e063107981fce84499b613bb
tobor <rosborne@osbornepro.com>
2025-12-07 18:14:53 -0500

    Update service_shared.go

--- cmd/novakey/service_shared.go
+++ cmd/novakey/service_shared.go
@@ -1,15 +1,30 @@
 package main
 
-import "log"
+import "github.com/sirupsen/logrus"
+
+func init() {
+	// Basic logrus setup; on Windows, eventlog_windows.go will attach
+	// an additional hook to forward logs to the Windows Event Log.
+	logrus.SetFormatter(&logrus.TextFormatter{
+		FullTimestamp: true,
+	})
+}
 
 func LogInfo(msg string) {
-	log.Println("[INFO]", msg)
+	logrus.Info(msg)
 }
 
 func LogError(msg string, err error) {
 	if err != nil {
-		log.Println("[ERROR]", msg, ":", err)
+		logrus.WithError(err).Error(msg)
 	} else {
-		log.Println("[ERROR]", msg)
+		logrus.Error(msg)
+	}
+}
+
+// zeroBytes overwrites a slice for security best-effort.
+func zeroBytes(b []byte) {
+	for i := range b {
+		b[i] = 0
 	}
 }
