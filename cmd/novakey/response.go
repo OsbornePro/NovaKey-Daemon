51baf37928991392e7dba49ff5244951cc316705
tobor <rosborne@osbornepro.com>
2025-12-21 23:11:18 -0700

    Update response.go

--- cmd/novakey/response.go
+++ cmd/novakey/response.go
@@ -1,4 +1,4 @@
-package novakey
+package main
 
 import (
 	"encoding/binary"
@@ -6,32 +6,35 @@ import (
 	"time"
 )
 
+const respVersion byte = 3
+
 type RespStatus byte
 
 const (
-	StatusOK            RespStatus = 0x00
-	StatusNotArmed      RespStatus = 0x01
-	StatusNeedsApprove  RespStatus = 0x02
-	StatusNotPaired     RespStatus = 0x03
-	StatusBadTimestamp  RespStatus = 0x04
-	StatusReplay        RespStatus = 0x05
-	StatusRateLimit     RespStatus = 0x06
-	StatusDecryptFailed RespStatus = 0x07
-	StatusInternal      RespStatus = 0x7F
+	StatusOK          RespStatus = 0x00
+	StatusNotArmed    RespStatus = 0x01
+	StatusNeedsApprove RespStatus = 0x02
+	StatusNotPaired   RespStatus = 0x03
+	StatusBadRequest  RespStatus = 0x04
+	StatusBadTimestamp RespStatus = 0x05
+	StatusReplay      RespStatus = 0x06
+	StatusRateLimit   RespStatus = 0x07
+	StatusCryptoFail  RespStatus = 0x08
+	StatusInternal    RespStatus = 0x7F
 )
 
-func writeResponse(conn net.Conn, status RespStatus, msg string) {
-	// Best-effort: old clients may close immediately; ignore write errors.
-	_ = conn.SetWriteDeadline(time.Now().Add(750 * time.Millisecond))
+func writeResp(conn net.Conn, st RespStatus, msg string) {
+	_ = conn.SetWriteDeadline(time.Now().Add(1 * time.Second))
 
 	b := []byte(msg)
 	if len(b) > 65535 {
 		b = b[:65535]
 	}
 
-	var hdr [3]byte
-	hdr[0] = byte(status)
-	binary.BigEndian.PutUint16(hdr[1:], uint16(len(b)))
+	var hdr [4]byte
+	hdr[0] = respVersion
+	hdr[1] = byte(st)
+	binary.BigEndian.PutUint16(hdr[2:], uint16(len(b)))
 
 	_, _ = conn.Write(hdr[:])
 	if len(b) > 0 {
