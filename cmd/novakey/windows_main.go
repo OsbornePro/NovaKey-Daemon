4bb6565fc1582807bc061410b966982e69c18185
tobor <rosborne@osbornepro.com>
2025-12-21 23:28:57 -0700

    Update windows_main.go

--- cmd/novakey/windows_main.go
+++ cmd/novakey/windows_main.go
@@ -65,6 +65,7 @@ func handleConnWin(reqID uint64, conn net.Conn, maxLen int) {
 	logReqf(reqID, "connection opened from %s", remote)
 
 	respond := func(st RespStatus, msg string) {
+		logReqf(reqID, "responding status=%d msg=%q", st, msg)
 		writeResp(conn, st, msg)
 	}
 
